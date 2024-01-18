13bbe4b8a7626a4c012a0c34f2b92771c33272ba
zozosy <zoubaida.taher17@gmail.com>
2024-01-17 20:32:35 -0800

    created child component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import BookList from './components/booklist';
+
 
 function App() {
   return (
    <>
    <h1>Zozo Book Store</h1>
    <p>Zozo Book Store, nestled in the city, offers a diverse collection from classics to contemporary reads. With a cozy aura and inviting reading nooks, it's a haven for book lovers.</p>
+   <BookList/>
    </>
   );
 }
---
+++ src/components/booklist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function BookList() {
+    return (  
+        <div>
+        <ul>
+         <li>Mind Platter</li>
+         <li>Shatter Me</li>
+         <li>Letter to the Person I Was</li>
+         </ul> 
+ 
+ 
+     </div>
+)};
