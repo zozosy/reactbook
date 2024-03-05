c00874c303b04b85bbedba67c311d69b38451740
zozosy <zoubaida.taher17@gmail.com>
2024-03-04 22:42:37 -0800

    Delete

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ function App() {
   const handleClick = (e) => { 
     setbookname([...bookname, e]);
   }
+  const handleRemove = (removeList) => {
+    setbookname(removeList);
+    };
   return (
    <>
    <h1>Zozo Book Store</h1>
    <p>Zozo Book Store, nestled in the city, offers a diverse collection from classics to contemporary reads. With a cozy aura and inviting reading nooks, it's a haven for book lovers.</p>
-   <BookList name = {bookname}/>
+   <BookList name = {bookname} onRemove = {handleRemove}/>
    <Addbookform addbook = {handleClick}/>
 
    </>
--- src/components/booklist.js
+++ src/components/booklist.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 export default function BookList(props) {
+    const handleRemove = (index) => {
+        const removeList = [...props.name];
+        removeList.splice(index, 1);
+        props.onRemove(removeList);
+        };
     return (
       <div>
          <ol>
          {props.name.map((book, i)=> (
-              <li key={i}>{book}</li>
+              <li key={i}>
+                {book} <button onClick={() => handleRemove(i)}> &#10060; </button> </li>
+              
+
           ))}
           </ol> 
-          
               
       </div>
     )
